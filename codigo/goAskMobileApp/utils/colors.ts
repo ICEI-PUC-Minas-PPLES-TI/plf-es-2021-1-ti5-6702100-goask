0d4f9db32b0f3b7db9e29e1ad026318d939c9d0b
Guicbdiniz <guicbdeniz@hotmail.com>
2021-03-24 20:15:23 -0300

    add app container rainbow

---
+++ codigo/goAskMobileApp/utils/colors.ts
@@ -0,0 +1,12 @@
+export const CONTAINER_STYLE_COLORS = {
+  RED: '#c9515c',
+  ORANGE: '#f27225',
+  DARK_GREEN: '#139882',
+  LIGHT_GREEN: '#32b24a',
+  DARK_BLUE: '#125daa',
+  LIGHT_BLUE: '#26265e',
+};
+
+export const COLORS = {
+  BACKGROUND: '#f3f3f3',
+};
