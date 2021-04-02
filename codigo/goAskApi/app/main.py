6319e878f209f5a8b1d30b1828a55c439100411c
Guilherme <guilhergoa@hotmail.com>
2021-04-02 20:50:23 -0300

    feat: add cors allow

--- codigo/goAskApi/app/main.py
+++ codigo/goAskApi/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 from app.routers.appRouter import app_routers
@@ -8,6 +9,20 @@ from app.db.database import engine
 load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(app_routers)
 
