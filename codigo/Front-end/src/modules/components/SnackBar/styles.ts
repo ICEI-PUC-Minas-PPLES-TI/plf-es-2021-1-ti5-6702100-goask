84637f493352200bdf486e3a9a547a8fdff2d1ae
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-04-07 18:52:12 -0300

    feat: box-shadow in snack bar

--- codigo/Front-end/src/modules/components/SnackBar/styles.ts
+++ codigo/Front-end/src/modules/components/SnackBar/styles.ts
@@ -17,6 +17,8 @@ export const Container = styled.div<Props>`
   align-items: center;
   color: #fff;
   padding: 10px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
   p {
     color: #fff;
     font-size: 18px;
