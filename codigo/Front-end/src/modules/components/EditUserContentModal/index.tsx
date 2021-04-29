8d9e09e83d93239f5dca42c1ab7b45a6d0766318
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-04-29 16:34:32 -0300

    fix: avatar default

--- codigo/Front-end/src/modules/components/EditUserContentModal/index.tsx
+++ codigo/Front-end/src/modules/components/EditUserContentModal/index.tsx
@@ -3,41 +3,38 @@ import React, { FunctionComponent } from "react";
 import EditUserInput from "../EditUser/EditUserInput";
 import ButtonForm from "../../components/ButtonForm";
 
-
 interface EditModalProps {
-  onConfirm: (e:React.FormEvent<HTMLFormElement>) => void;
+  onConfirm: (e: React.FormEvent<HTMLFormElement>) => void;
   name: string;
   email: string;
 }
 
-const EditContentModal: FunctionComponent<EditModalProps> = (
-  props
-) => {
+const EditContentModal: FunctionComponent<EditModalProps> = (props) => {
   return (
     <React.Fragment>
-        <form onSubmit = {props.onConfirm}>
+      <form onSubmit={props.onConfirm}>
         <styles.Container>
-                  <EditUserInput
-                    type="text"
-                    icon="/user.svg"
-                    placeHolder="novo nome"
-                    alt="Nome"
-                    name="username"
-                    value={props.name}
-                  />
-                  <EditUserInput
-                    type="text"
-                    icon="/mail-icon.svg"
-                    placeHolder="novo e-mail"
-                    alt="E-mail"
-                    name="email"
-                    value={props.email}
-                  />{" "}
-                  <styles.ButtonDiv>
-                    <ButtonForm icon="/arrow-right.svg" alt="Enviar" />
-                  </styles.ButtonDiv>
-                </styles.Container>
-        </form>
+          <EditUserInput
+            type="text"
+            icon="/user.svg"
+            placeHolder="novo nome"
+            alt="Nome"
+            name="username"
+            value={props.name}
+          />
+          <EditUserInput
+            type="text"
+            icon="/mail-icon.svg"
+            placeHolder="novo e-mail"
+            alt="E-mail"
+            name="email"
+            value={props.email}
+          />{" "}
+          <styles.ButtonDiv>
+            <ButtonForm icon="/arrow-right.svg" alt="Enviar" />
+          </styles.ButtonDiv>
+        </styles.Container>
+      </form>
     </React.Fragment>
   );
 };
